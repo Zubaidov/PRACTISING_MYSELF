09a194fd7f0cd54379e3119671ff011122a5208f
Zubaidov <shohruz.zubaidov@gmail.com>
2022-10-10 01:20:09 +0400

    last updates

--- pytest_folder/helloworld.py
+++ pytest_folder/helloworld.py
@@ -14,7 +14,7 @@ com1.config()
 com2.config() 
 '''
 
-class Computer:
+'''class Computer:
     
     def __init__(self):
         self.name = "hello"
@@ -35,3 +35,33 @@ c1.update()
 
 print(c1.name, c1.age)
 print(c2.name, c2.age)
+'''
+
+class Computer:
+    def __init__(self):
+        self.name = "Hello world"
+        self.age = 5
+
+    def compare(self, other):
+        if self.age == other.age:
+            return True
+        else:
+            return False
+        
+
+
+c1 = Computer()
+c1.name = "C1"
+c1.age = 25
+c2 = Computer()
+c2.name = "C2"
+c2.age = 20
+
+
+if c1.compare(c2):
+    print("They are same")
+else:
+    print("they are different") 
+
+print(c1.name)
+print(c2.name)
